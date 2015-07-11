ed571be3d76f08df9c4bca8cfc817363df144969
willbenica <w.benica@gmail.com>
2015-07-11 12:15:46 -0400

    Created a dict with all weapons, wepon damages and descriptions.

---
+++ weapons.py
@@ -0,0 +1,33 @@
+# weapons.py
+
+# A dict of all the weapons in the game. First level are the weapon names, with sub dicts with the 'Damage' and a 'Description'
+weapons = {
+    'Dull Sword': {
+        'Damage': 5,
+        'Description': 'This was all that our poor hero could afford with what\nwas in his pocket.'
+    },
+    'Illuminati Sword': {
+        'Damage': 23,
+        'Description': 'A geometricly patterend sword. With a pyrimd around an\neye on the hilt and cross-bar.'
+    },
+    'Improbable Sword': {
+        'Damage': 42,
+        'Description': 'This sword doesn\'t make any sense. The blade is dull\nand the hilt is sharp. Across the dull blade are inscribed various\nideograms: a whale, a potted plant and the words don\'t panic.'
+    },
+    'Hyrulian Sword': {
+        'Damage': 100,
+        'Description': 'This sword is exquisite! Three triangles glow with\ngolden light.'
+    },
+    'Gnashing Teeth': {
+        'Damage': 2,
+        'Description': 'Rotten, broken, sharp and pointy teeth.'
+    },
+    'Dwarfish Sword': {
+        'Damage': 15,
+        'Description': 'A beutifully inlaid sword, set with gems.'
+    },
+    'Dragon Breath': {
+        'Damage': 33,
+        'Description': 'The magical breath of a dragon. It melts metal and\nburns word. Water boils when a dragon breath near to it.'
+    }
+}
