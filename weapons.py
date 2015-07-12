dfcaa2f1d4c0392906a97d12e88912003b5860ea
willbenica <w.benica@gmail.com>
2015-07-12 10:57:09 -0400

    Added a 'name' attribute, and all attribute names are lower case.

--- weapons.py
+++ weapons.py
@@ -3,31 +3,38 @@
 # A dict of all the weapons in the game. First level are the weapon names, with sub dicts with the 'Damage' and a 'Description'
 weapons = {
     'Dull Sword': {
-        'Damage': 5,
-        'Description': 'This was all that our poor hero could afford with what\nwas in his pocket.'
+        'name': 'Dull Sword',
+        'damage': 5,
+        'description': 'This was all that our poor hero could afford with what\nwas in his pocket.'
     },
     'Illuminati Sword': {
-        'Damage': 23,
-        'Description': 'A geometricly patterend sword. With a pyrimd around an\neye on the hilt and cross-bar.'
+        'name': 'Illuminati Sword',
+        'damage': 23,
+        'description': 'A geometricly patterend sword. With a pyrimd around an\neye on the hilt and cross-bar.'
     },
     'Improbable Sword': {
-        'Damage': 42,
-        'Description': 'This sword doesn\'t make any sense. The blade is dull\nand the hilt is sharp. Across the dull blade are inscribed various\nideograms: a whale, a potted plant and the words don\'t panic.'
+        'name': 'Improbable Sword',
+        'damage': 42,
+        'description': 'This sword doesn\'t make any sense. The blade is dull\nand the hilt is sharp. Across the dull blade are inscribed various\nideograms: a whale, a potted plant and the words don\'t panic.'
     },
     'Hyrulian Sword': {
-        'Damage': 100,
-        'Description': 'This sword is exquisite! Three triangles glow with\ngolden light.'
+        'name': 'Hyrulian Sword',
+        'damage': 100,
+        'description': 'This sword is exquisite! Three triangles glow with\ngolden light.'
     },
     'Gnashing Teeth': {
-        'Damage': 2,
-        'Description': 'Rotten, broken, sharp and pointy teeth.'
+        'name': 'Gnashing Teeth',
+        'damage': 2,
+        'description': 'Rotten, broken, sharp and pointy teeth.'
     },
     'Dwarfish Sword': {
-        'Damage': 15,
-        'Description': 'A beutifully inlaid sword, set with gems.'
+        'name': 'Dwarfish Sword',
+        'damage': 15,
+        'description': 'A beutifully inlaid sword, set with gems.'
     },
     'Dragon Breath': {
-        'Damage': 33,
-        'Description': 'The magical breath of a dragon. It melts metal and\nburns word. Water boils when a dragon breath near to it.'
+        'name': 'Dragon Breath',
+        'damage': 33,
+        'description': 'The magical breath of a dragon. It melts metal and\nburns word. Water boils when a dragon breath near to it.'
     }
 }
